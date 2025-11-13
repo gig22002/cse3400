d206dbe8d591c69c8b87ebb69c05cb0b454df5bf
gio <gianpaolo.girasoli@uconn.edu>
2025-11-13 13:14:41 -0500

    move to cryptographic functions

--- primitivestest.py
+++ primitivestest.py
@@ -1,6 +1,6 @@
 import secrets
 import random
-from primePy import primes
+from primePy import primes as p
 
 def PRG1(_in):
     #32-bit PRG
@@ -15,12 +15,16 @@ def PRG2(_in):
     random.seed(b)
     return format(random.getrandbits(_blen), f"{_blen}b")
 
+def GetRandPrime():
+    pList = p.between(10000,100000)
+    return pList[random.randint(0,len(pList))]
+
 def PRF1(k, _in):
-    raise NotImplementedError
-    out=PRG2(_in)
-    return format(out%k, "32b")
+    return (k%GetRandPrime())^_in
     
 
 if __name__=="__main__":
+    k = random.getrandbits(32)
+    print(k)
     print(PRG2(420))
-    print(PRF1(10,420))
+    print(PRF1(k,420))
